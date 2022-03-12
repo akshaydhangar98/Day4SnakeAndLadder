f14fe6ad598c79bed0d093081058698ed01103b1
akshay dhangar <akshaygdhangar14@gmail.com>
2022-03-12 12:14:57 +0530

    Dice roll Code

---
+++ UC2_DiceThrow.java
@@ -0,0 +1,12 @@
+package day4snakeandladder;
+
+public class UC2_DiceThrow {
+
+	public static void main(String[] args) {
+		
+			
+			int randomCheck=(int) Math.floor(Math.random() * 10) % 6+1;
+			System.out.println(+randomCheck);
+	}
+
+}
